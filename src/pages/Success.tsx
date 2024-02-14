eb4c63db7e417d442e26422acb02a9bf4fdfbbbc
tijo <81065469+artijo@users.noreply.github.com>
2024-02-14 23:14:25 +0700

    Create Success.tsx

---
+++ src/pages/Success.tsx
@@ -0,0 +1,10 @@
+import Layout from "../Layout"
+export default function Success() {
+    return (
+        <Layout title="Success">
+            <div className="mt-5">
+                <h1 className="text-2xl font-bold text-gray-900">Success</h1>
+            </div>
+        </Layout>
+    )
+}

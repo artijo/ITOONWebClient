2af17dde39c229e46977e26af47c2d18e3455ff6
tijo <81065469+artijo@users.noreply.github.com>
2024-02-27 20:48:44 +0700

    delete profile page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Login from './pages/Login'
 import UploadCartoon from './pages/UploadCartoon'
 import UploadEpisode from './pages/UploadEpisode'
 import Error from './pages/Error'
-import Profile from './pages/Profile'
 import Coi from './pages/CoinTransaction'
 import Success from './pages/Success'
 import ApproveCreator from './pages/AppoveCreator'
@@ -23,7 +22,6 @@ function App() {
         <Route path="/upload/episode/:id" element={<UploadEpisode />} />
         <Route path="/edit/episode/:episode" element={<UploadEpisode />} />
         <Route path="/error" element={<Error/>}/>
-        <Route path="/profile" element={<Profile/>} />
         <Route path="/success" element={<Success message="อัพโหลดสำเร็จ"/>} />
         <Route path="/coin-transaction" element={<Coi/>} />
         <Route path='/coin-transaction/success' element={<Success message="ชำระเงินเรียบร้อยโปรดกลับไปที่แอปและเช็คเหรียญของคุณ"/>} />
